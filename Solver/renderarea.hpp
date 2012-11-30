6b6039e0cf977555017d4eb7ed88921621cd1f1c
ifsmirnov <ifsmirnov@yandex.ru>
2012-11-30 12:03:47 +0400

    RenderArea setDesktop slot added

--- Solver/renderarea.hpp
+++ Solver/renderarea.hpp
@@ -9,11 +9,12 @@ class RenderArea : public QWidget
     Q_OBJECT
 public:
     explicit RenderArea(QWidget *parent = 0);
-
-    void setImage(QImage);
     void paintEvent(QPaintEvent *);
-
     QSize sizeHint() const;
+    void setImage(QImage);
+
+public slots:
+    void setDesktop();
 
 private:
     QImage image;
